2d71e2497eaf3c2983586497011de7af3e8fb53e
collumww <collumww@yahoo.com>
2020-06-20 12:55:15 -0400

    fooling around. not needed.

--- ss/ssRange.cs
+++ ss/ssRange.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ss {
+namespace ss { // what?
     public struct ssRange {
         public ssRange(int left, int right) {
             l = left;

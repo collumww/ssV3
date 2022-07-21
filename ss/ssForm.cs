c674d85ab6f4d2d830bc566855ac9ff0f6e3e711
collumww <collumww@yahoo.com>
2022-07-21 08:39:10 -0400

    git test

--- ss/ssForm.cs
+++ ss/ssForm.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Reflection;
 using System.IO;
-
+//
 namespace ss {
     public partial class ssForm : Form {
         public ssForm(ssEd e, ssText t) {
